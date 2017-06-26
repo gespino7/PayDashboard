37c98eb2adb8159df017bf80a5773af47b3938a5
Gerardo <gespino7@ucsc.edu>
2017-06-26 16:39:21 -0700

    UI updated Button are now big enough for visibility, also included a textview for to display the check

--- TamalesDash/ViewController.swift
+++ TamalesDash/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ 
+    @IBOutlet weak var item1: UIButton!
+    @IBOutlet weak var item2: UIButton!
+    @IBOutlet weak var item3: UIButton!
+    @IBOutlet weak var item4: UIButton!
+    @IBOutlet weak var item5: UIButton!
+    @IBOutlet weak var item6: UIButton!
+    @IBOutlet weak var item7: UIButton!
+    @IBOutlet weak var item8: UIButton!
+    @IBOutlet weak var item9: UIButton!
+    @IBOutlet weak var item10: UIButton!
+    @IBOutlet weak var item11: UIButton!
+    @IBOutlet weak var item12: UIButton!
+    @IBOutlet weak var item13: UIButton!
+    @IBOutlet weak var item14: UIButton!
+    @IBOutlet weak var item15: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
