ca6d45a0bb0c6eca23e94cea08fbd0bc07ee1a7c
Gerardo <gespino7@ucsc.edu>
2017-06-27 13:03:58 -0700

    items can be added to tableview

--- TamalesDash/Item.swift
+++ TamalesDash/Item.swift
@@ -2,21 +2,27 @@
 //  Item.swift
 //  TamalesDash
 //
-//  Created by Machine on 6/24/17.
+//  Created by Machine on 6/27/17.
 //  Copyright © 2017 Gerardo Espinoza. All rights reserved.
 //
 
 import UIKit
 
-class Item: NSObject {
+class Item: UITableViewCell {
 
-    var price:Float
-    let name:String
-    var countTotal:Int
+    public  var name:String = ""
+    public  var price:Double = 0.0
+    public  var count:Int = 0
     
-    init(name:String, price:Float, countTotal:Int){
-        self.name = name
-        self.price = price
-        self.countTotal = countTotal
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
     }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
 }
--- TamalesDash/ViewController.swift
+++ TamalesDash/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,
+                        UITextFieldDelegate{
 
  
     @IBOutlet weak var item1: UIButton!
@@ -26,60 +27,202 @@ class ViewController: UIViewController {
     @IBOutlet weak var item13: UIButton!
     @IBOutlet weak var item14: UIButton!
     @IBOutlet weak var item15: UIButton!
+    @IBOutlet weak var sendOrder: UIButton!
     
+    @IBOutlet weak var tableView: UITableView!
+ 
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+  
     
     @IBAction func item1Touched(_ sender: Any) {
-        print("Puerco")
+        let tempName = item1.titleLabel?.text
+        let tempItem = Item()
+        tempItem.name = tempName!
+        orderItems.append(tempItem)
+        debugPrint(tempName!)
+        self.tableView.reloadData()
+      
     }
     @IBAction func item2Touched(_ sender: Any) {
+        let tempName = item2.titleLabel?.text
+        let tempItem = Item()
+        tempItem.name = tempName!
+        orderItems.append(tempItem)
+        debugPrint(tempName!)
+        self.tableView.reloadData()
     }
     
     @IBAction func item3Touched(_ sender: Any) {
+        let tempName = item3.titleLabel?.text
+        let tempItem = Item()
+        tempItem.name = tempName!
+        orderItems.append(tempItem)
+        debugPrint(tempName!)
+        self.tableView.reloadData()
     }
     
     @IBAction func item4Touched(_ sender: Any) {
+        let tempName = item4.titleLabel?.text
+        let tempItem = Item()
+        tempItem.name = tempName!
+        orderItems.append(tempItem)
+        debugPrint(tempName!)
+        self.tableView.reloadData()
     }
 
     @IBAction func item5Touched(_ sender: Any) {
+        let tempName = item5.titleLabel?.text
+        let tempItem = Item()
+        tempItem.name = tempName!
+        orderItems.append(tempItem)
+        debugPrint(tempName!)
+        self.tableView.reloadData()
     }
     
     @IBAction func item6Touched(_ sender: Any) {
+        let tempName = item6.titleLabel?.text
+        let tempItem = Item()
+        tempItem.name = tempName!
+        orderItems.append(tempItem)
+        debugPrint(tempName!)
+        self.tableView.reloadData()
+
     }
     
     @IBAction func item7Touched(_ sender: Any) {
+        let tempName = item7.titleLabel?.text
+        let tempItem = Item()
+        tempItem.name = tempName!
+        orderItems.append(tempItem)
+        debugPrint(tempName!)
+        self.tableView.reloadData()
+
     }
     
     
     @IBAction func item8Touched(_ sender: Any) {
+        let tempName = item8.titleLabel?.text
+        let tempItem = Item()
+        tempItem.name = tempName!
+        orderItems.append(tempItem)
+        debugPrint(tempName!)
+        self.tableView.reloadData()
+
     }
     
     @IBAction func item9Touched(_ sender: Any) {
+        let tempName = item9.titleLabel?.text
+        let tempItem = Item()
+        tempItem.name = tempName!
+        orderItems.append(tempItem)
+        debugPrint(tempName!)
+        self.tableView.reloadData()
+
     }
     
     @IBAction func item10Touched(_ sender: Any) {
+        let tempName = item10.titleLabel?.text
+        let tempItem = Item()
+        tempItem.name = tempName!
+        orderItems.append(tempItem)
+        debugPrint(tempName!)
+        self.tableView.reloadData()
+
     }
     
     @IBAction func item11Touched(_ sender: Any) {
+        let tempName = item11.titleLabel?.text
+        let tempItem = Item()
+        tempItem.name = tempName!
+        orderItems.append(tempItem)
+        debugPrint(tempName!)
+        self.tableView.reloadData()
+
     }
     
     @IBAction func item12Touched(_ sender: Any) {
+        let tempName = item12.titleLabel?.text
+        let tempItem = Item()
+        tempItem.name = tempName!
+        orderItems.append(tempItem)
+        debugPrint(tempName!)
+        self.tableView.reloadData()
+
     }
     
     @IBAction func item13Touched(_ sender: Any) {
+        let tempName = item13.titleLabel?.text
+        let tempItem = Item()
+        tempItem.name = tempName!
+        orderItems.append(tempItem)
+        debugPrint(tempName!)
+        self.tableView.reloadData()
+
     }
     
     @IBAction func item14Touched(_ sender: Any) {
+        let tempName = item14.titleLabel?.text
+        let tempItem = Item()
+        tempItem.name = tempName!
+        orderItems.append(tempItem)
+        debugPrint(tempName!)
+        self.tableView.reloadData()
     }
     
     @IBAction func item15Touched(_ sender: Any) {
+        let tempName = item15.titleLabel?.text
+        let tempItem = Item()
+        tempItem.name = tempName!
+        orderItems.append(tempItem)
+        debugPrint(tempName!)
+        self.tableView.reloadData()
+
     }
+    
+    @IBAction func sendOrder(_ sender: Any) {
+        let name = sendOrder.titleLabel?.text
+        orderItems.removeAll()
+        self.tableView.reloadData()
+        debugPrint(name!)
+    }
+    
+    
+    var orderItems = [Item]()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return orderItems.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = orderItems[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        debugPrint("Select item \(indexPath.row)")
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            orderItems.remove(at: indexPath.row)
+            tableView.deleteRows(at:  [indexPath], with: .fade)
+        }
+    }
+    
+    
+    
+    
     
     
     override func didReceiveMemoryWarning() {
